a2bdf3322fa65c839f414d487775ad0714f6ab1b
Dmitriy Kondakov <diman4life@mail.ru>
2024-04-10 18:38:18 +0300

    initial commit

--- app/component/page.jsx
+++ app/component/page.jsx
@@ -1,26 +1,26 @@
-import ItemsAccordion from '../components/Accordion'
-import styles from './component.module.css'
+import ItemsAccordion from "../components/Accordion";
+import styles from "./component.module.css";
 
 const items = [
-    {
-      summary: 'Overview',
-      details: `Cypress also allows you to test individual components of your app via
+  {
+    summary: "Overview",
+    details: `Cypress also allows you to test individual components of your app via
       component tests`,
-      id: '1',
-    },
-    {
-        summary: 'Component Vs E2E',
-        details: `Cypress Component Testing uses the same test runner, commands, 
+    id: "1",
+  },
+  {
+    summary: "Component Vs E2E",
+    details: `Cypress Component Testing uses the same test runner, commands, 
         and API to test components instead of pages. However, the main difference
         is that Cypress Component Testing builds your components using a development 
         server instead of rendering within a complete website, which results in 
         faster tests and fewer dependencies on infrastructure than end-to-end 
         tests covering the same code paths`,
-        id: '2',
-      },
-      {
-        summary: 'Additional configuration',
-        details: `You may need to add additional configuration to your project to support
+    id: "2",
+  },
+  {
+    summary: "Additional configuration",
+    details: `You may need to add additional configuration to your project to support
         component tests. For example, In Next JS you'll need to add something like: 
         
         const { defineConfig } = require('cypress')
@@ -33,21 +33,21 @@ const items = [
             },
           },
         })`,
-        id: '3'
-      },
-      {
-        summary: 'Example',
-        details: `You can mount a component using cy.mount(). Then, you can interact and 
+    id: "3",
+  },
+  {
+    summary: "Example",
+    details: `You can mount a component using cy.mount(). Then, you can interact and 
         make assertions just as you would in an E2E test. Let's go test our Accordion`,
-        id: '4'
-      }
-]
+    id: "4",
+  },
+];
 
-export default function ComponentPage(){
-    return (
-        <main className={styles.main}>
-            <h1 className={styles.header}>Component Testing</h1>
-            <ItemsAccordion items={items} />
-        </main>
-    )
+export default function ComponentPage() {
+  return (
+    <main className={styles.main}>
+      <h1 className={styles.header}>Component Testing</h1>
+      <ItemsAccordion items={items} />
+    </main>
+  );
 }
